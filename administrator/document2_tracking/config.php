f5c58054122b11b8de0f4751333b5522c0818942
Dumrus Sukasam <dumrus.sukasam@unicity.com>
2020-01-10 06:09:50 +0700

    เพิ่มเอกสารติดตามสถานะงาน

---
+++ administrator/document2_tracking/config.php
@@ -0,0 +1,17 @@
+<META content="text/html; charset=utf-8" http-equiv=Content-Type>
+<?php 
+	$PK_field = "id";
+	//$FR_field = "";
+	$check_module = "อัฟโหลดเอกสาร SV";
+	$page_name = "เอกสารติดตามสถานะงาน";
+	$tbl_name = "s_document_tracking";
+	$field_confirm_showname= "doc_name";
+	$fieldlist = array('images','fo_id','target','doc_name','sorts','status');
+	$search_key = array('doc_name');
+	$title_del="เอกสารติดตามสถานะงาน";
+	$title_del_name="doc_name";
+	$pagesize = 20;
+	$pages="เอกสารติดตามสถานะงาน";
+
+	$a_param = array('page','orderby','sortby','keyword','pagesize','mid','smid');
+?>
