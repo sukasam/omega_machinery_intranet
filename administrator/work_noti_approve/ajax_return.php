32cfc7417ad3d5503e89242fbd7c499718f8b1f6
Dumrus Sukasam <dumrus.sukasam@unicity.com>
2020-02-18 15:13:38 +0700

    เพิ่มระบบอนุมติใบแจ้งงาน

---
+++ administrator/work_noti_approve/ajax_return.php
@@ -0,0 +1,150 @@
+<?php    
+	include_once("../../include/aplication_top.php");
+	header("Content-type: text/html; charset=windows-874");
+	header("Cache-Control: no-cache, must-revalidate");
+	@mysqli_query($conn,"SET NAMES tis620");
+	
+	if($_GET['action'] == 'getproDetail'){
+		$pid = $_GET['pro_id'];
+		$rowpro  = @mysqli_fetch_array(@mysqli_query($conn,"SELECT * FROM s_group_typeproduct WHERE group_id = '".$pid."'"));
+
+		$amount = '0';
+		$listPRo = '';
+		$qupro1 = @mysqli_query($conn,"SELECT * FROM s_group_typeproduct ORDER BY group_name ASC");
+		while($row_qupro1 = @mysqli_fetch_array($qupro1)){
+
+			$chkL = ($pid == $row_qupro1['group_id']) ? 'selected' : '';
+			$listPRo .='<option value="'.$row_qupro1['group_id'].'" '.$chkL.'>'.$row_qupro1['group_name'].'</option>';
+		    	
+		}
+
+		echo "|".$rowpro['group_spro_id']."|".$listPRo."|".$amount;
+	}
+	
+	if($_GET['action'] == 'getprice'){
+		$pid = $_GET['pid'];
+		$prid = $_GET['prid'];
+		$rowpro  = @mysqli_fetch_array(@mysqli_query($conn,"SELECT * FROM s_group_typeproduct WHERE group_id = '".$pid."'"));
+		$sumprice = $prid * $rowpro['group_pro_price'];
+		echo number_format($sumprice);
+	}
+	
+	if($_GET['action'] == 'getcus'){
+		$cd_name =  iconv( 'UTF-8', 'TIS-620', $_REQUEST['pval']);
+		if($cd_name != ""){
+			$consd = "WHERE cd_name LIKE '%".$cd_name."%'";
+		}
+		//echo "SELECT * FROM s_first_order ".$consd." ORDER BY cd_name ASC";
+		$qu_cus = mysqli_query($conn,"SELECT * FROM s_first_order ".$consd." ORDER BY cd_name ASC");
+		while($row_cus = @mysqli_fetch_array($qu_cus)){
+			?>
+			 <tr>
+				<td><A href="javascript:void(0);" onclick="get_customer('<?php echo $row_cus['fo_id'];?>');"><?php echo $row_cus['cd_name']." (".$row_cus['loc_name'].")";?></A></td>
+			  </tr>
+			<?php    	
+		}
+		//echo "SELECT cd_name FROM s_work_noti ".$consd." ORDER BY cd_name ASC";
+	}
+
+	if($_GET['action'] == 'get_pros'){
+		$cd_name =  iconv( 'UTF-8', 'TIS-620', $_REQUEST['pval']);
+		if($cd_name != ""){
+			$consd = "WHERE 1 AND (group_name LIKE '%".$cd_name."%' OR group_spro_id LIKE '%".$cd_name."%')";
+		}
+		$col = $_REQUEST['col'];
+		//echo "SELECT * FROM s_group_typeproduct ".$consd." ORDER BY group_name ASC";
+		$qu_cus = mysqli_query($conn,"SELECT * FROM s_group_typeproduct ".$consd." ORDER BY group_name ASC");
+		while($row_pros = @mysqli_fetch_array($qu_cus)){
+			?>
+			 <tr>
+				<td><A href="javascript:void(0);" onclick="get_products('<?php echo $row_pros['group_id'];?>','<?php echo $col;?>');"><?php  echo $row_pros['group_spro_id']." | ".$row_pros['group_name'];?></A></td>
+			  </tr>
+			<?php    	
+		}
+		//echo "SELECT cd_name FROM s_work_noti ".$consd." ORDER BY cd_name ASC";
+	}
+
+
+	if($_GET['action'] == 'getcusDetail'){
+
+		$cus_id =  iconv( 'UTF-8', 'TIS-620', $_REQUEST['cus_id']);
+		$qu_cus = mysqli_query($conn,"SELECT * FROM s_first_order WHERE 1 AND `fo_id` = '".$cus_id."' LIMIT 1");
+		$row_cus = @mysqli_fetch_array($qu_cus);
+
+		$cProvince = '';
+		$quprovince = @mysqli_query($conn,"SELECT * FROM s_province ORDER BY province_id ASC");
+		while($row_province = @mysqli_fetch_array($quprovince)){
+
+			$CpChk = ($row_cus['cd_province'] == $row_province['province_id']) ? 'selected':'';
+			$cProvince .= '<option value="'.$row_province['province_id'].'" '.$CpChk.'>'.$row_province['province_name'].'</option>';	
+		}
+		$cSale = '';
+		$CsTel = '';
+		$qusaletype = @mysqli_query($conn,"SELECT * FROM s_group_sale ORDER BY group_name ASC");
+		while($row_saletype = @mysqli_fetch_array($qusaletype)){
+
+			$CsChk = ($row_cus['cs_company'] == $row_saletype['group_id']) ? 'selected':'';
+			if($row_cus['cs_company'] == $row_saletype['group_id']){
+				$CsTel = $row_saletype['group_tel'];
+			}
+			$cSale .= '<option value="'.$row_saletype['group_id'].'" '.$CsChk.'>'.$row_saletype['group_name'].'</option>';  	
+		}
+
+		echo "|".$row_cus['cd_name']."|".$row_cus['cd_address']."|".$cProvince."|".$row_cus['c_contact']."|".$row_cus['c_tel']."|".$row_cus['loc_name']."|".$row_cus['loc_address']."|".$cSale.'|'.$CsTel;
+
+	}
+
+	
+	
+	if($_GET['action'] == 'getpodkey'){
+		$cd_name =  iconv( 'UTF-8', 'TIS-620', $_REQUEST['pval']);
+		$keys = $_REQUEST['keys'];
+		if($cd_name != ""){
+			$consd = "WHERE group_name LIKE '%".$cd_name."%'";
+		}
+		//echo "SELECT group_name FROM s_group_typeproduct ".$consd." ORDER BY group_name ASC";
+		$qu_cus = mysqli_query($conn,"SELECT * FROM s_group_pod ".$consd." ORDER BY group_name ASC");
+		while($row_cus = @mysqli_fetch_array($qu_cus)){
+			?>
+			 <tr>
+				<td><A href="javascript:void(0);" onclick="get_pod('<?php     echo $row_cus['group_id'];?>','<?php     echo $row_cus['group_name'];?>','<?php     echo $keys;?>');"><?php     echo $row_cus['group_name'];?></A></td>
+			  </tr>
+			<?php    	
+		}
+		//echo "SELECT cd_name FROM s_work_noti ".$consd." ORDER BY cd_name ASC";
+	}
+	
+	if($_GET['action'] == 'getprotype'){
+		$group_id = $_REQUEST['group_id'];
+		$group_name = $_REQUEST['group_name'];
+		$protype = $_REQUEST['protype'];
+		
+		$listPRo = '';
+		$qupro1 = @mysqli_query($conn,"SELECT * FROM s_group_typeproduct ORDER BY group_name ASC");
+		while($row_qupro1 = @mysqli_fetch_array($qupro1)){
+			if($group_id == $row_qupro1['group_id']){
+				$listPRo .= '<option value="'.$row_qupro1['group_id'].'" selected="selected">'.$row_qupro1['group_name'].'</option>';
+			}else{
+				$listPRo .= '<option value="'.$row_qupro1['group_id'].'">'.$row_qupro1['group_name'].'</option>';
+			}
+		}
+		echo '|'.$listPRo.'|'.get_pro_code($conn,$group_id);
+	}
+	
+	if($_GET['action'] == 'getpod'){
+		$group_id = $_REQUEST['group_id'];
+		$group_name = $_REQUEST['group_name'];
+		$protype = $_REQUEST['protype'];
+		
+		$qupros1 = @mysqli_query($conn,"SELECT * FROM s_group_pod ORDER BY group_name ASC");
+		while($row_qupros1 = @mysqli_fetch_array($qupros1)){
+		  ?>
+			<option value="<?php     echo $row_qupros1['group_id'];?>" <?php     if($group_id == $row_qupros1['group_id']){echo 'selected';}?>><?php     echo $row_qupros1['group_name'];?></option>
+		  <?php    	
+		}
+
+		//echo "SELECT * FROM s_group_typeproduct ORDER BY group_name ASC";
+	}
+
+?>
+
--- administrator/work_noti_approve/config.php
+++ administrator/work_noti_approve/config.php
@@ -2,14 +2,14 @@
 <?php    
 	$PK_field = "fo_id";
 	//$FR_field = "";
-	$check_module = "ใบสั่งงาน/แจ้งงาน";
-	$page_name = "ใบสั่งงาน/แจ้งงาน";
+	$check_module = "อนุมัติใบสั่งงาน/แจ้งงาน";
+	$page_name = "อนุมัติใบสั่งงาน/แจ้งงาน";
 	$tbl_name = "s_work_noti";
 	$field_confirm_showname= "group_name";
 	$fieldlist = array('cd_name','cd_address','cd_province','cd_contact','cd_position','cd_mobile','cd_line','cd_tel','cd_email','sale_contact','sale_tel','fs_id','date_open','loc_name','loc_address','work_detail1','work_detail2','work_detail3','work_detail4','work_detail5','date_work1','date_work2','date_work3','date_work4','date_work5','work_list','sign_work1','sign_work2','sign_work3','sign_date_work1','sign_date_work2','sign_date_work3','images1','images2','images3','remark','get_noti','status','approve');
 	$search_key = array('cd_name','cd_address','cd_province','cd_tel','cd_email','fs_id','date_open','remark','status');
 	$pagesize = 50;
-	$pages="ใบสั่งงาน/แจ้งงาน";
+	$pages="อนุมัติใบสั่งงาน/แจ้งงาน";
 
 	$a_param = array('page','orderby','sortby','keyword','pagesize','mid','smid');
 ?>
