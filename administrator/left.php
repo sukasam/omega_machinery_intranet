342dd2141677c977e7545c577364fbd5cf78773e
Dumrus Sukasam <dumrus.sukasam@unicity.com>
2021-03-11 16:00:39 +0700

    Updated Code.

--- administrator/left.php
+++ administrator/left.php
@@ -8,7 +8,7 @@
             <li style="padding-top:20px;"><a href="../group_stockmachine/"><img src="../images/menu/stock_machine.png" width="129" height="103" border="0" alt=""></a></li>
             <li style="padding-top:20px;"><a href="../bill_lading/"><img src="../images/menu/bill_lading_shipping.png" width="140" height="103" border="0" alt=""></a></li>
             <li style="padding-top:20px;"><a href="../report_chk/?mid=16"><img src="../images/menu/report.png" width="88" height="125" border="0" alt=""></a></li>
-            <li style="padding-top:20px;"><a href="../quotation3"><img src="../images/menu/quotation.png" width="129" border="0" alt=""></a></li>
+            <li style="padding-top:20px;"><a href="../quotation_chk"><img src="../images/menu/quotation.png" width="129" border="0" alt=""></a></li>
             <li style="padding-top:20px;"><a href="../scanqr/index.php"><img src="../images/menu/scan_qrcode.png" width="125" height="100" border="0" alt=""></a></li>
             <li style="padding-top:20px;"><a href="../setting"><img src="../images/menu/setting.png" width="75" height="105" border="0" alt=""></a></li>
             <li style="padding-top:20px;padding-bottom:20px;"><a href="../logout.php"><img src="../images/menu/logout.png" width="70" height="97" border="0" /></a></li>
