4ecb66dc57f242b887fe54b031f7f6039d372edd
Dumrus.S <dumrus.sukasam@unicity.com>
2021-10-30 09:55:33 +0700

    Updated Connect database

--- include/connect.php
+++ include/connect.php
@@ -1,6 +1,6 @@
 <?php  
 	
-        $conn = mysqli_connect("localhost", "root", "", "omega_machinery_intranet");
+        $conn = mysqli_connect("localhost", "root", "", "omegaint_machinery");
 	@mysqli_query($conn,"SET NAMES UTF8");
 
 //        $conn = mysqli_connect("localhost", "omegaint_machinery", "LImsdgjWu", "omegaint_machinery");
